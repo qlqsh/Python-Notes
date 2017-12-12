c3ca3e2bd822bcde6c1201c99314522173f52fab
qlqsh <hopefor@gmail.com>
2017-12-12 16:25:58 +0800

    json

---
+++ json/example_1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+from urllib.request import urlopen
+
+def getCountry(ipAddress):
+    """通过IP地址获取国家信息"""
+    response = urlopen("http://freegeoip.net/json/"+ipAddress).read().decode('utf-8')
+    responseJson = json.loads(response)
+    return responseJson.get("country_code")
+
+# print(getCountry("50.78.253.58"))
+
+jsonString = '''
+{
+    "arrayOfNums": [
+        {"number":0}, 
+        {"number":1}, 
+        {"number":2}
+    ],
+    "arrayOfFruits": [
+        {"fruit": "apple"},
+        {"fruit": "banana"},
+        {"fruit": "pear"}
+    ]
+}
+'''
+jsonObj = json.loads(jsonString)
+
+print(jsonObj.get("arrayOfNums"))
+print(jsonObj.get("arrayOfNums")[1])
+print(jsonObj.get("arrayOfNums")[1].get("number")+jsonObj.get("arrayOfNums")[2].get("number"))
+print(jsonObj.get("arrayOfFruits")[2].get("fruit"))
