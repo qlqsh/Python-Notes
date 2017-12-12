03e305df43420ee435365eacdd193de63df73c32
qlqsh <hopefor@gmail.com>
2017-12-12 10:07:25 +0800

    更新爬虫

--- Scrapy/wikiSpider/wikiSpider/spiders/articleSpider2.py
+++ Scrapy/wikiSpider/wikiSpider/spiders/articleSpider2.py
@@ -6,14 +6,18 @@ from wikiSpider.items import Article
 from scrapy.linkextractors import LinkExtractor
 
 class ArticleSpider(CrawlSpider):
-    name = "article2"    
+    # 爬虫名称
+    name = "article2"
+    # 允许的主机域名
     allowed_domains = ["en.wikipedia.org"]
+    # 爬取的URL数组
     start_urls = ["http://en.wikipedia.org/wiki/Python_%28programming_language%29"]
+    # 规则。提取指定链接。
     rules = [Rule(LinkExtractor(allow=('(/wiki/)((?!:).)*%'), ), callback="parse_item", follow=True)]
-    
+    # 爬取指定内容。
     def parse_item(self, response):
         item = Article()
-        # TODO: 这里有点问题，有的页面不止是h1里面嵌套i，所以就报错了。
+        # 这里有点问题，有的页面不止是h1里面嵌套i，所以就报错了。
         title = response.xpath('//h1/text()')[0].extract()
         print("Title is: " + title)
         item['title'] = title
