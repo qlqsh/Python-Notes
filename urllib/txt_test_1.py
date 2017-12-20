f57d89c00ff38afeaa5910ef0d0c223c3b344ca2
qlqsh <hopefor@gmail.com>
2017-12-20 15:25:06 +0800

    更新读取txt。涉及编码。

--- urllib/txt_test_1.py
+++ urllib/txt_test_1.py
@@ -2,5 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from urllib.request import urlopen
-textPage = urlopen("http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt")
-print(str(textPage.read(), 'utf-8'))
+from bs4 import BeautifulSoup
+
+# textPage = urlopen("http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt")
+# print(str(textPage.read(), 'utf-8'))
+
+try:
+    html = urlopen("http://en.wikipedia.org/wiki/Python_(programming_language)")
+except URLError as e:
+    print(e)
+except HTTPError as e:
+    print(e)
+bsObj = BeautifulSoup(html, "html.parser")
+content = bsObj.find("div", {"id": "mw-content-text"}).get_text()
+# print(content)
+
+# 感觉下面的转换很没有意义。除非bytes里面不是utf-8，而是iso编码，可能就有必要了。
+content = bytes(content, "UTF-8")
+# print(content)
+content = content.decode("UTF-8")
+# print(content)
