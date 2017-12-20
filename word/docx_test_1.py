890ae56e6d481559a0d21ea458a143d4acba4fa6
qlqsh <hopefor@gmail.com>
2017-12-20 21:27:39 +0800

    读取word实例修改

--- word/docx_test_1.py
+++ word/docx_test_1.py
@@ -12,9 +12,9 @@ except URLError as e:
     print(e)
 except HTTPError as e:
     print(e)
-wordFile = BytesIO(wordFile)
-document = ZipFile(wordFile)
-xml_content = document.read('word/document.xml')
+wordFile = BytesIO(wordFile) # 文件流
+document = ZipFile(wordFile) # 解压文档
+xml_content = document.read('word/document.xml') # 获取解压后的xml文件。
 
 wordObj = BeautifulSoup(xml_content.decode('utf-8'), "lxml-xml")
 textStrings = wordObj.findAll("w:t")
